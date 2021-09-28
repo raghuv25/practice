c64b79b3baa4f9da37c8c882ec0c9648c54f588f
V <Raghunath_V@McAfee.com>
2021-09-28 12:14:03 +0530

    initial comit

---
+++ src/Amazon/demo.java
@@ -0,0 +1,14 @@
+package Amazon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");  
+		WebDriver driver=new ChromeDriver();
+
+	}
+
+}
